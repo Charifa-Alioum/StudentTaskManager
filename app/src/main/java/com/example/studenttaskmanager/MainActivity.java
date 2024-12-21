d2301decf48e1b8d5f51cfc95aefc830028ff705
Charifa Alioum <alioumcharifa307@gmail.com>
2024-12-21 22:36:03 +0100

    Commit #1: Création du projet de base

---
+++ app/src/main/java/com/example/studenttaskmanager/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.studenttaskmanager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
+    Toolbar toolbar;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        toolbar = findViewById(R.id.toolbar);
+        fab = findViewById(R.id.fab);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView=findViewById(R.id.navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId= item.getItemId();
+                if(itemId==R.id.bottom_timetable){
+                    openFragment(new TimetableFragment());
+                    return true;
+                } else if(itemId==R.id.bottom_agenda){
+                    openFragment(new AgendaFragment());
+                    return true;
+                } else if(itemId==R.id.bottom_subjects){
+                    openFragment(new SubjectFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        fragmentManager=getSupportFragmentManager();
+        openFragment(new OverviewFragment());
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Overview pages", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_overview) {
+            openFragment(new OverviewFragment());
+        } else if (itemId == R.id.nav_agenda) {
+            openFragment(new AgendaFragment());
+        } else if (itemId == R.id.nav_timetable) {
+            openFragment(new TimetableFragment());
+        } else if (itemId == R.id.nav_modules) {
+            openFragment(new ModuleFragment());
+        } else if (itemId == R.id.nav_subjects) {
+            openFragment(new SubjectFragment());
+        } else if (itemId == R.id.nav_homework) {
+            openFragment(new HomeworkFragment());
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed () {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+    private void openFragment(Fragment fragment){
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.commit();
+    }
+}
